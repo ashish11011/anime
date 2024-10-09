f3fea1f5313f15288b5c86a92382bf4fd8ffab7e
Ashish Bishnoi <ashish.bishnoi@silverrev.com>
2024-10-09 12:26:12 +0530

    navbar done

--- src/components/navBar.tsx
+++ src/components/navBar.tsx
@@ -1,33 +1,78 @@
+import { Menu, X } from "lucide-react";
 import Link from "next/link";
+import { useState } from "react";
 
 const NavBar = () => {
+  const [mobNavOpen, setMobNavOpen] = useState(false);
+
   return (
-    <div className="w-full px-4 md:px-8 py-6 md:py-4 items-center  flex gap-8 justify-between">
-      <Link href={"/"} className=" min-w-14 max-h-8 md:max-h-none h-10 w-auto">
-        <img className="h-full w-full object-contain" src="./logo.png" alt="" />
+    <div className="w-full sticky top-0 z-50 bg-black px-4 md:px-8 py-6 md:py-4 items-center flex gap-8 justify-between">
+      <Link href={"/"} className="min-w-14 max-h-8 md:max-h-none h-10 w-auto">
+        <img
+          className="h-full w-full object-contain"
+          src="https://s3.ap-south-1.amazonaws.com/cozzy.corner/logo.png"
+          alt=""
+        />
       </Link>
-      <div className="">
-        <ul className=" border border-neutral-700 rounded-full overflow-hidden text-white flex">
+
+      <div className="hidden md:block">
+        <ul className="border border-neutral-700 rounded-full overflow-hidden text-white flex">
           <Link
             href={"/category/naruto"}
-            className=" md:text-base px-4 md:px-5 py-1.5 text-sm hover:bg-neutral-800 cursor-pointer duration-300 font-semibold "
+            className="md:text-base px-4 md:px-5 py-1.5 text-sm hover:bg-neutral-800 cursor-pointer duration-300 font-semibold"
           >
             Naruto
           </Link>
           <Link
             href={"/category/anime"}
-            className=" md:text-base px-4 md:px-5 py-1.5 text-sm hover:bg-neutral-800 cursor-pointer duration-300 font-semibold "
+            className="md:text-base px-4 md:px-5 py-1.5 text-sm hover:bg-neutral-800 cursor-pointer duration-300 font-semibold"
           >
             Anime
           </Link>
           <Link
             href={"/category/posters"}
-            className=" md:text-base px-4 md:px-5 py-1.5 text-sm hover:bg-neutral-800 cursor-pointer duration-300 font-semibold "
+            className="md:text-base px-4 md:px-5 py-1.5 text-sm hover:bg-neutral-800 cursor-pointer duration-300 font-semibold"
           >
             Posters
           </Link>
         </ul>
       </div>
+
+      <div className="block md:hidden">
+        {mobNavOpen ? (
+          <X onClick={() => setMobNavOpen(false)} color="#ccc" />
+        ) : (
+          <Menu onClick={() => setMobNavOpen(true)} color="#ccc" />
+        )}
+      </div>
+
+      {mobNavOpen && (
+        <div className="absolute top-20 left-0 w-full h-screen bg-black text-white flex items-center justify-center ">
+          <div className="flex flex-col items-center justify-center z-40 -translate-y-24">
+            <Link
+              href={"/category/naruto"}
+              className="text-xl py-3 hover:bg-neutral-800 w-full text-center"
+              onClick={() => setMobNavOpen(false)}
+            >
+              Naruto
+            </Link>
+            <Link
+              href={"/category/anime"}
+              className="text-xl py-3 hover:bg-neutral-800 w-full text-center"
+              onClick={() => setMobNavOpen(false)}
+            >
+              Anime
+            </Link>
+            <Link
+              href={"/category/posters"}
+              className="text-xl py-3 hover:bg-neutral-800 w-full text-center"
+              onClick={() => setMobNavOpen(false)}
+            >
+              Posters
+            </Link>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
---
+++ src/const/products.tsx
@@ -0,0 +1,104 @@
+export const productsData = [
+  {
+    name: "Itachi Uchiha",
+    images: [
+      "https://s3.ap-south-1.amazonaws.com/cozzy.corner/IMG-20241009-WA0002.jpg",
+      "https://s3.ap-south-1.amazonaws.com/cozzy.corner/IMG-20241009-WA0003.jpg",
+      "https://s3.ap-south-1.amazonaws.com/cozzy.corner/IMG-20241009-WA0007.jpg",
+    ],
+    rating: 3,
+    price: 800,
+    discountPrice: 280,
+    description:
+      "Embrace the power and mystery of Itachi Uchiha with this captivating figure. Perfect for Naruto fans and collectors.",
+    productHeadlines: [
+      "Intricate detailing for ultimate realism",
+      "High-quality collectible for Naruto fans",
+      "Discounted price for a limited time",
+      "Perfect for any anime display or collection",
+    ],
+    id: "b107fe68-15cd-4139-aae4-027465f895df",
+  },
+  {
+    name: "Itachi Uchiha",
+    images: [
+      "https://s3.ap-south-1.amazonaws.com/cozzy.corner/IMG-20241009-WA0005.jpg",
+      "https://s3.ap-south-1.amazonaws.com/cozzy.corner/IMG-20241009-WA0012.jpg",
+      "https://s3.ap-south-1.amazonaws.com/cozzy.corner/IMG-20241009-WA0013.jpg",
+      "https://s3.ap-south-1.amazonaws.com/cozzy.corner/IMG-20241009-WA0014.jpg",
+      "https://s3.ap-south-1.amazonaws.com/cozzy.corner/IMG-20241009-WA0015.jpg",
+    ],
+    rating: 5,
+    price: 800,
+    discountPrice: 280,
+    description:
+      "This larger Itachi Uchiha figure stands out in any collection, showcasing the iconic character’s signature look.",
+    productHeadlines: [
+      "Multiple high-resolution images included",
+      "Perfect for serious anime figure collectors",
+      "Premium quality with attention to details",
+      "Unique pose and design, a must-have for Naruto lovers",
+    ],
+    id: "8b0237e8-1c52-426e-96a5-865693aa89d7",
+  },
+  {
+    name: "Captain America",
+    images: [
+      "https://s3.ap-south-1.amazonaws.com/cozzy.corner/IMG-20241009-WA0018.jpg",
+      "https://s3.ap-south-1.amazonaws.com/cozzy.corner/IMG-20241009-WA0004.jpg",
+      "https://s3.ap-south-1.amazonaws.com/cozzy.corner/IMG-20241009-WA0017.jpg",
+    ],
+    rating: 5,
+    price: 800,
+    discountPrice: 280,
+    description:
+      "Bring home the hero of justice with this high-quality Captain America figure, an iconic addition to any Marvel collection.",
+    productHeadlines: [
+      "Highly detailed and durable craftsmanship",
+      "A must-have for any Marvel fan",
+      "Perfect for collectors and superhero enthusiasts",
+      "Great for display or gifting",
+    ],
+    id: "4005ac31-a8b4-4f63-a3c7-8f51487cc66c",
+  },
+  {
+    name: "Monkey D. Luffy",
+    images: [
+      "https://s3.ap-south-1.amazonaws.com/cozzy.corner/IMG-20241009-WA0008.jpg",
+      "https://s3.ap-south-1.amazonaws.com/cozzy.corner/IMG-20241009-WA0011.jpg",
+      "https://s3.ap-south-1.amazonaws.com/cozzy.corner/IMG-20241009-WA0010.jpg",
+    ],
+    rating: 4,
+    price: 800,
+    discountPrice: 280,
+    description:
+      "Set sail with Monkey D. Luffy, the king of pirates, in this fantastic figure perfect for any One Piece fan.",
+    productHeadlines: [
+      "Accurately replicates Luffy's iconic outfit",
+      "Perfect addition for One Piece lovers",
+      "Exquisite craftsmanship with vibrant colors",
+      "Highly collectible with a special discount",
+    ],
+    id: "1b9a352b-a39a-4d28-aaf1-cc9206ed7260",
+  },
+  {
+    name: "Mubco Star Wars Bobble Head",
+    images: [
+      "https://s3.ap-south-1.amazonaws.com/cozzy.corner/IMG-20241009-WA0020.jpg",
+      "https://s3.ap-south-1.amazonaws.com/cozzy.corner/IMG-20241009-WA0021.jpg",
+      "https://s3.ap-south-1.amazonaws.com/cozzy.corner/IMG-20241009-WA0019.jpg",
+    ],
+    rating: 5,
+    price: 800,
+    discountPrice: 280,
+    description:
+      "This quirky Star Wars bobblehead adds fun to your collection, making it a perfect gift for fans of the galaxy far, far away.",
+    productHeadlines: [
+      "Unique Star Wars character bobblehead",
+      "Perfect collectible for Star Wars fans",
+      "High-quality materials with fun design",
+      "Ideal for home, office, or display",
+    ],
+    id: "5147ac05-6d5d-45d1-b484-ee465cff4cd7",
+  },
+];
