c82f00e79a8f13a7c572b38905c3b571e76b827f
Ashish Bishnoi <bishnoi11011@gmail.com>
2024-10-23 18:32:43 +0530

    show category and series

--- src/app/category/[id]/page.tsx
+++ src/app/category/[id]/page.tsx
@@ -1,20 +1,18 @@
-'use client';
-
 import Footer from '@/components/footer';
 import NavBar from '@/components/navBar';
-import { productsData } from '@/const/products';
-import Link from 'next/link';
-import { useState } from 'react';
+import ProductCard from '@/components/productCard';
+import connect from '@/dbConfig/dbConfig';
+import Product from '@/Models/productModel';
 
-export default function Page(context: any) {
+export default async function Page(context: any) {
   const params = context.params;
 
   // Decode the URL-encoded parameter to get the original value
   const decodedCategory = decodeURIComponent(params.id);
 
-  const categoryItems = productsData.filter(
-    (product) => product.category === decodedCategory
-  );
+  await connect();
+
+  const categoryItems = await Product.find({ category: decodedCategory });
 
   return (
     <div className="flex min-h-screen w-full flex-col bg-neutral-950">
@@ -35,7 +33,7 @@ const AnimeCardList = ({ productsData }: any) => {
   return (
     <div className="mx-auto grid w-full max-w-7xl grid-cols-1 gap-6 p-8 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
       {productsData.map((character: any, index: number) => (
-        <AnimeCard
+        <ProductCard
           key={character.id}
           id={character.id}
           name={character.name}
@@ -45,68 +43,6 @@ const AnimeCardList = ({ productsData }: any) => {
           discountPrice={character.discountPrice}
         />
       ))}
-    </div>
-  );
-};
-
-const AnimeCard = ({ id, name, images, rating, price, discountPrice }: any) => {
-  const [isAdded, setIsAdded] = useState(false);
-
-  return (
-    <div
-      // href={`/product/${id}`}
-      className="group transform rounded-lg border border-gray-600 bg-neutral-950 p-3 shadow-lg transition-transform duration-300"
-    >
-      <img
-        src={images[0]}
-        alt={name}
-        className="block h-52 w-full rounded-lg bg-white object-contain duration-300 group-hover:hidden"
-      />
-      <img
-        src={images[1]}
-        alt={name}
-        className="hidden h-52 w-full rounded-lg bg-white object-contain duration-300 group-hover:block"
-      />
-
-      <div className="flex flex-col gap-0 py-4">
-        {/* Name */}
-        <h3 className="line-clamp-1 text-xl font-bold text-gray-100">{name}</h3>
-
-        {/* Price and Discount */}
-        <div className="">
-          <span className="text-lg text-neon-blue line-through">₹{price}</span>
-          <span className="ml-2 text-lg font-bold text-p-green">
-            ₹{discountPrice}
-          </span>
-        </div>
-
-        <Link
-          href={`/product/${id}`}
-          className="mt-4 w-full rounded-lg border py-2 text-center font-medium text-white transition duration-300 hover:bg-white hover:text-gray-800"
-        >
-          View product
-        </Link>
-
-        {/* Shop Button */}
-        {!isAdded ? (
-          <div
-            // onClick={() =>
-            //   addToCart({
-            //     id,
-            //     name,
-            //     price: discountPrice,
-            //     image: images[0],
-            //     quantity: 1,
-            //   })
-            // }
-            className="mt-4 w-full cursor-pointer rounded-lg border py-2 text-center font-medium text-white transition duration-300 hover:bg-white hover:text-gray-800"
-          >
-            Add to cart
-          </div>
-        ) : (
-          <div className=""></div>
-        )}
-      </div>
     </div>
   );
 };
--- src/app/series/[id]/page.tsx
+++ src/app/series/[id]/page.tsx
@@ -1,12 +1,8 @@
-'use client';
-
 import Footer from '@/components/footer';
 import NavBar from '@/components/navBar';
-import { productsData } from '@/const/products';
+import ProductCard from '@/components/productCard';
 import connect from '@/dbConfig/dbConfig';
 import Product from '@/Models/productModel';
-import Link from 'next/link';
-
 export default async function Page(context: any) {
   const params = context.params;
 
@@ -14,7 +10,7 @@ export default async function Page(context: any) {
   const decodedCategory = decodeURIComponent(params.id);
 
   await connect();
-  const categoryItems = await Product.find({ series: decodedCategory });
+  const productsData = await Product.find({ series: decodedCategory });
 
   return (
     <div className="flex min-h-screen w-full flex-col bg-neutral-950">
@@ -24,7 +20,7 @@ export default async function Page(context: any) {
         <div className="flex max-w-xl flex-col gap-4 px-8 text-5xl font-medium text-white">
           <p className="font-bold capitalize">{decodedCategory}</p>{' '}
         </div>
-        <AnimeCardList productsData={categoryItems} />
+        <AnimeCardList productsData={productsData} />
       </div>
       <Footer />
     </div>
@@ -35,7 +31,7 @@ const AnimeCardList = ({ productsData }: any) => {
   return (
     <div className="mx-auto grid w-full max-w-7xl grid-cols-1 gap-6 p-8 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
       {productsData.map((character: any, index: number) => (
-        <AnimeCard
+        <ProductCard
           key={character.id}
           id={character.id}
           name={character.name}
@@ -46,43 +42,5 @@ const AnimeCardList = ({ productsData }: any) => {
         />
       ))}
     </div>
-  );
-};
-
-const AnimeCard = ({ id, name, images, rating, price, discountPrice }: any) => {
-  return (
-    <Link
-      href={`/product/${id}`}
-      className="group transform rounded-lg border border-gray-600 bg-neutral-950 p-3 shadow-lg transition-transform duration-300"
-    >
-      <img
-        src={images[0]}
-        alt={name}
-        className="block h-52 w-full rounded-lg bg-white object-contain duration-300 group-hover:hidden"
-      />
-      <img
-        src={images[1]}
-        alt={name}
-        className="hidden h-52 w-full rounded-lg bg-white object-contain duration-300 group-hover:block"
-      />
-
-      <div className="flex flex-col gap-0 py-4">
-        {/* Name */}
-        <h3 className="text-xl font-bold text-gray-100">{name}</h3>
-
-        {/* Price and Discount */}
-        <div className="">
-          <span className="text-lg text-neon-blue line-through">₹{price}</span>
-          <span className="ml-2 text-lg font-bold text-p-green">
-            ₹{discountPrice}
-          </span>
-        </div>
-
-        {/* Shop Button */}
-        <div className="mt-4 w-full rounded-lg border py-2 text-center font-medium text-white transition duration-300 hover:bg-white hover:text-gray-800">
-          Add to cart
-        </div>
-      </div>
-    </Link>
   );
 };
