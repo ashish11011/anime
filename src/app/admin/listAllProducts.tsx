0f1d63b2084e5df2a7103e1559a3441b9940bd6d
Ashish Bishnoi <ashish.bishnoi@silverrev.com>
2024-10-23 18:32:43 +0530

    show category and series

--- src/app/admin/listAllProducts.tsx
+++ src/app/admin/listAllProducts.tsx
@@ -1,11 +1,30 @@
 'use client';
 
+import { Blocks, EllipsisVertical, Pencil, Trash2 } from 'lucide-react';
 import Link from 'next/link';
+import { useRouter } from 'next/navigation';
 import React, { useState } from 'react';
 
 const ListAllProducts = ({ productData }: any) => {
   const [showProducts, setShowProducts] = useState(true);
   const products = JSON.parse(productData);
+  const router = useRouter();
+
+  async function handleOutOfStock(id: any) {
+    const data = await fetch('/api/outOfStock', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ id }),
+    });
+
+    if (data.ok) {
+      // window.location.reload();
+      console.log('Toggled out of stock');
+      router.refresh();
+    }
+  }
 
   return (
     <div className="mx-auto flex w-[72rem] flex-col gap-6">
@@ -28,7 +47,9 @@ const ListAllProducts = ({ productData }: any) => {
         <div className="hide-scrollbar mx-auto flex w-full flex-col overflow-x-scroll">
           {products.map((product: any, index: number) => {
             return (
-              <div className="grid grid-cols-10 gap-2 border px-2 py-1">
+              <div
+                className={`grid grid-cols-10 gap-2 border px-2 py-1 hover:bg-gray-50 ${product?.outOfStock && 'bg-red-300'} `}
+              >
                 <div className="col-span-1 flex items-center justify-between gap-1">
                   <p>{index + 1}</p>
                   <div className="size-12 overflow-hidden">
@@ -41,10 +62,25 @@ const ListAllProducts = ({ productData }: any) => {
                 </div>
 
                 <p className="col-span-3 flex items-center">{product.name}</p>
-                <p>{product.price}</p>
-                <p>{product.discountPrice}</p>
-                <p>{product.category}</p>
-                <p>{product.series}</p>
+                <p className="self-center">{product.price}</p>
+                <p className="self-center">{product.discountPrice}</p>
+                <p className="self-center">{product.category}</p>
+                <p className="self-center">{product.series}</p>
+                <p className="flex items-center gap-3">
+                  <Pencil
+                    className="cursor-pointer text-gray-600 duration-200 hover:scale-110 hover:text-gray-800"
+                    size={20}
+                  />
+                  {/* <Trash2
+                    className="cursor-pointer text-gray-600 duration-200 hover:scale-110 hover:text-gray-800"
+                    size={20}
+                  /> */}
+                  <Blocks
+                    onClick={() => handleOutOfStock(product.id)}
+                    className="cursor-pointer text-gray-600 duration-200 hover:scale-110 hover:text-gray-800"
+                    size={20}
+                  />
+                </p>
               </div>
             );
           })}
