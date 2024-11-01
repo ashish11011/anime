d12a70d060d94437a0b640d6fdd40a6b38efe8f9
Ashish Bishnoi <bishnoi11011@gmail.com>
2024-11-01 12:21:42 +0530

    changed cashe ot admin orders

--- src/app/admin/orders/page.tsx
+++ src/app/admin/orders/page.tsx
@@ -3,6 +3,8 @@ import OrderModel from '@/Models/orderModel';
 import React from 'react';
 import ListAllTheOrders from './listAllTheOrders';
 
+export const revalidate = 0;
+
 const Orders = async () => {
   await connect();
   const orderDataString = await OrderModel.find({});
